534af52b4fd26c5bb350c448238620bd12ce5c9f
digitalaltar <moon@digitalaltar.com>
2023-04-13 19:06:11 -0700

    Removing video for now at least

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -12,22 +12,3 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
   });
 });
-
-document.addEventListener('DOMContentLoaded', function () {
-  const viewVideoLinks = document.querySelectorAll('.view-video');
-
-  viewVideoLinks.forEach(link => {
-    link.addEventListener('click', function (event) {
-      event.preventDefault();
-
-      const godContainer = event.target.closest('.god');
-      const image = godContainer.querySelector('.god-img');
-      const videoContainer = godContainer.querySelector('.video-container');
-
-      image.style.display = 'none';
-      videoContainer.style.display = 'block';
-
-      video.play();
-    });
-  });
-});
